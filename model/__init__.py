2ffcdc3d9cc2c49f03141a275380ef69edfae6e8
patrick <patrick@ubuntu.ubuntu-domain>
2014-09-16 08:52:22 +0800

    db config

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,7 @@
 import MySQLdb
 from sae.const import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
-
+from flask import Flask, g
+app  = Flask(__name__); 
 db = MySQLdb.connect(
         MYSQL_HOST,
         MYSQL_USER,
@@ -8,3 +9,5 @@ db = MySQLdb.connect(
         MYSQL_DB,
         port=int(MYSQL_PORT)
         )
+g.conn = db;
+g.cursor = db.cursor;
