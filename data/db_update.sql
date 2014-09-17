47a97bee68ff4abbf9e1badcc5398bc7c27df689
Patrick <hzxa21@126.com>
2014-09-18 03:19:24 +0800

    basic function implemented

---
+++ data/db_update.sql
@@ -0,0 +1,66 @@
+drop table if exists `student`;
+CREATE TABLE student
+(
+	`username` VARCHAR(128) NOT NULL comment '用户名',
+	`grade` VARCHAR(32) NOT NULL comment '年级',
+	`nickname` VARCHAR(128) NULL comment '昵称',
+	`password` VARCHAR(128) NOT NULL comment '密码',
+    `major` varchar(32) null comment '专业名称',
+    `school` varchar(32) null comment '所属学院',
+    `user_id` varchar(32) not null comment '学号',
+	`email` VARCHAR(128) NULL comment '邮箱',
+	`mail` VARCHAR(128) NULL comment '邮编',
+	`call` VARCHAR(128) NULL comment '联系电话',
+	`signature` VARCHAR(256) NULL comment '签名',
+	`pre_school` VARCHAR(256) NULL comment '原学校',
+	`address` VARCHAR(256) NULL comment '地址',
+	`detail_address` VARCHAR(512) NULL comment '详细地址',
+	`pic_url` VARCHAR(256) NULL comment '头像链接',
+	unique key(`username`),
+	primary key (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='学生表';
+
+drop table if exists `course`;
+CREATE TABLE `course`
+(
+    `cou_id` varchar(32) not null comment '课程resourceID',
+    `course_name` varchar(512) not null comment '课程名称',
+    `time` varchar(1024) not null comment '上课时间',
+    `teacher` VARCHAR(32) not null comment '授课老师',
+	primary key (`cou_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='课程表';
+
+drop table if exists `stu_course`;
+CREATE TABLE `stu_course`
+(
+	`sc_id` INTEGER NOT NULL AUTO_INCREMENT comment '主键',
+    `stu_id` INTEGER not null REFERENCES `student` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `cou_id` INTEGER not null REFERENCES `course` (`cou_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	primary key (`sc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='学生-课程表';
+
+
+drop table if exists `post`;
+CREATE TABLE post
+(
+	`post_id` INTEGER NOT NULL AUTO_INCREMENT comment '主键',
+	`title` VARCHAR(128) NOT NULL comment '标题',
+	`content` TEXT NOT NULL comment '帖子内容',
+	`tags` TEXT NULL comment '标签',
+	`status` INTEGER NOT NULL default 0 comment '状态，0 = 草稿，1 = 发布',
+    `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`author_id` INTEGER NOT NULL REFERENCES student(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	`cou_id` INTEGER NOT NULL REFERENCES `course` (`cou_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	primary key (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='日志表';
+
+drop table if exists `comments`;
+CREATE TABLE `comments`
+(
+	`com_id` INTEGER NOT NULL AUTO_INCREMENT comment '主键',
+	`content` TEXT NOT NULL comment '评论内容',
+    `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`author_id` INTEGER NOT NULL REFERENCES `student` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	`post_id` INTEGER NOT NULL REFERENCES `post` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	primary key (`com_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='评论表';
---
+++ route.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding=utf-8
+from flask import request, g,session,render_template,url_for,redirect
+from app import app
+from api import sysucourse
+
+def query_db(query, args=(), one=False):
+    g.cursor.execute(query,args)
+    rv = [dict((g.cursor.description[idx][0], value)
+        for idx, value in enumerate(row)) for row in g.cursor.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+def fetch_course():
+    res, course_info = sysucourse.get_course(session['USR_COOKIE'],'2014-2015','2')
+    if not res:
+        session.pop('USR_COOKIE')
+        return redirect(url_for('login'))
+    else:
+        for item in course_info:
+            course_item = query_db("SELECT * FROM course WHERE cou_id = %s",(item['cou_id'],),one=True)
+            if not course_item:
+                g.cursor.execute("""INSERT INTO course(cou_id,course_name,time,teacher) 
+                    VALUES(%s,%s,%s,%s)""",(item['cou_id'],item['course_name'],item['time'],item['teacher']))
+            g.cursor.execute("INSERT INTO stu_course(stu_id,cou_id) VALUES(%s,%s)",
+                             (session['USR_ID'],item['cou_id']))
+        g.conn.commit()
+        return course_info
+
+@app.route('/')
+def index():
+    if 'USR_ID' in session:
+        return render_template('user.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    
+    """
+        form传值变量名：
+            stuid：学号
+            password：密码
+
+        返回值：
+            error：验证是否出错（True表示出错)
+    """
+    if request.method == 'POST':    
+        res, cookie = sysucourse.login(request.form['stuid'],request.form['password'])
+        res2, info = sysucourse.get_student_info(cookie)
+        if res and res2:
+            user =query_db("SELECT * FROM student WHERE user_id = %s",(request.form['stuid'],),one=True)
+            if user==None:
+                g.cursor.execute(
+                    "INSERT INTO student(user_id,username,school,major,grade) VALUES(%s,%s,%s,%s,%s)",
+                    (request.form['stuid'],info['name'],info['school'],info['major'],info['grade']))
+            g.conn.commit()
+            # store user's cookie of sysujwxt
+            session['USR_COOKIE'] = cookie;
+            session['USR_ID'] = request.form['stuid']
+            
+            return redirect(url_for('user'))
+        else:
+            return render_template('login.html',error = True)    #how to set the para for failure
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('USR_ID')
+    session.pop('USR_COOKIE')
+    return redirect(url_for('login'))
+
+@app.route('/course',methods = ['GET','POST'])
+def course():
+    """
+        form传值变量名：
+            year：学年度
+            term：学期
+
+        返回值：
+            course_list: 学生课程列表，每个课程为一个字典
+    """
+    if 'USR_ID' in session:
+        if request.method == 'GET':
+            course_info = query_db("SELECT * FROM stu_course a,course b WHERE a.cou_id = b.cou_id and a.stu_id = %s",
+                                  (session['USR_ID'],))
+            if not course_info:
+                course_info = fetch_course()
+            return render_template('course.html',course_list = course_info) #how to set the default para
+        else:
+            course_info = fetch_course()
+            return render_template('course.html',course_list = course_info)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/user',methods = ['GET','POST'])
+def user():
+    """
+        form传值变量名：
+            email,call,signature,mail,pre_school,address,detail_address,nickname
+
+        返回值：
+            user_info: 用户个人信息的字典
+    """
+    if 'USR_ID' in session:
+        if request.method == 'GET':
+            user_info = query_db("SELECT * FROM student WHERE user_id = %s ",(session['USR_ID'],), one = True)
+            print user_info
+            return render_template('user.html', user_info = user_info)
+        else:
+           if g.cursor.execute("""UPDATE SET nickname = %s,email = %s,mail = %s,call =  %s,
+                        signature = %s ,pre_school=%s,address=%s,detail_address=%s""",
+                               (request.form['nickname'],request.form['email'],request.form['mail'],
+                               request.form['call'],request.form['signature'],request.form['pre_school'],
+                               request.form['address'],request.form['detail_address'])):
+               g.conn.commit()
+               return redirect(url_for('user'))
+           else:
+               return render_template('user.html',error = True)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/course/<course_id>', methods = ['GET','POST'])
+def course_page(course_id):
+    """
+        form传值变量名：
+            title:标题
+            content:内容
+
+        返回值：
+            post_list: 课程帖子列表，每一个帖子是一个字典
+    """
+    if 'USR_ID' in session:
+        if request.method == 'GET':
+            post_info = query_db("SELECT * FROM post a, student b WHERE a.author_id=b.user_id and cou_id = %s",(course_id,))
+            return render_template('course_page.html',post_list = post_info,cou_id = str(course_id))
+        else:
+            g.cursor.execute("INSERT INTO post(cou_id,title,content,author_id) VALUES(%s,%s,%s,%s)",
+                            (course_id,request.form['title'],request.form['content'],session['USR_ID']))
+            g.conn.commit()
+            return redirect('/course/' + course_id)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/course/<course_id>/post')
+def post(course_id):
+    if 'USR_ID' in session:
+        return render_template('post.html',cou_id = course_id)
+
+@app.route('/discussion/<discussion_id>/post')
+def comment(discussion_id):
+    if 'USR_ID' in session:
+        return render_template('comment.html',post_id = discussion_id)
+
+
+@app.route('/discussion/<discussion_id>', methods = ['GET','POST'])
+def discussion_page(discussion_id):
+    """
+        form传值变量名：
+
+        返回值：
+            comment_list: 帖子评论列表，每一个评论是一个字典
+    """
+    if 'USR_ID' in session:
+        if request.method == 'GET':
+            comment_info = query_db("SELECT * FROM comments a,student  b WHERE a.author_id=b.user_id and post_id = %s",(discussion_id,))
+            post_info = query_db("SELECT * FROM post a,  student b WHERE a.author_id=b.user_id and  post_id = %s",(discussion_id,),one=True)
+            return render_template('discussion_page.html',comment_list = comment_info,post_info = post_info,post_id = discussion_id)
+        else:
+            g.cursor.execute("INSERT INTO comments(post_id,content,author_id) VALUES(%s,%s,%s)",
+                            (discussion_id,request.form['content'],session['USR_ID']))
+            g.conn.commit()
+            return redirect('/discussion/' + str(discussion_id))
+    else:
+        return redirect(url_for('login'))
