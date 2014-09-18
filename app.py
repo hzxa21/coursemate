02996e7a9ee03aa1ab018f6b248133324b2c3c73
CJK-Regan <regan.cjk@gmail.com>
2014-09-18 20:05:30 +0800

    Connect db every request.

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, g
 import MySQLdb
 from sae.const import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
 
-db = MySQLdb.connect(
+app = Flask(__name__)
+app.secret_key = 'vkFwRjPgASwNed1sBmXjy+o/OSY0qFsPe1fljeyDE5aSbidWesAp9tNcenWcnmnS'
+app.debug = True
+
+@app.before_request
+def init():
+    g.conn = MySQLdb.connect(
             MYSQL_HOST,
             MYSQL_USER,
             MYSQL_PASS,
@@ -10,15 +16,7 @@ db = MySQLdb.connect(
             port=int(MYSQL_PORT),
             charset="utf8"
             )
-
-app = Flask(__name__)
-app.secret_key = 'vkFwRjPgASwNed1sBmXjy+o/OSY0qFsPe1fljeyDE5aSbidWesAp9tNcenWcnmnS'
-app.debug = True
-
-@app.before_request
-def init():
-    g.conn = db
-    g.cursor = db.cursor()
+    g.cursor = g.conn.cursor()
 
 import route
 
